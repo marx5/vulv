fd9a9f13e744f06cefb21992775e0356e9f814d5
VuLV <vulv.bnvn@gmail.com>
2026-08-24 09:45:35 +0000

    VuLV

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="vu-lv-container">
+      <h1 className="vu-lv-title">VuLV</h1>
+    </div>
+  )
+}
+
+export default App
